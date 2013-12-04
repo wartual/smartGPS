54e428586e4d6fd2c461b667bc091d95354314d1
wartual <zeljkoplesac@yahoo.com>
2013-12-04 19:33:03 +0100

    poceo raditi modele

---
+++ smartGPS.Business/Models/Foursquare/Icons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace smartGPS.Business.Models.Foursquare
+{
+    public class Icons
+    {
+        [JsonProperty("prefix")]
+        public String Prefix { get; set; }
+
+        [JsonProperty("sufx")]
+        public String Sufix { get; set; }
+    }
+}
