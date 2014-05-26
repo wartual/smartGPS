579e2c7f263f9050cf142440f381f9b14baabf42
wartual <zeljkoplesac@yahoo.com>
2014-05-27 00:01:09 +0200

    bug fixing, new deploy

--- smartGPS/Models/UserAdministration/ApiExternalLoginModel.cs
+++ smartGPS/Models/UserAdministration/ApiExternalLoginModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace smartGPS.Models.UserAdministration
 {
     public class APIExternalLoginModel
     {
+        [JsonProperty("username")]
         public String username { get; set; }
 
+        [JsonProperty("password")]
         public String password { get; set; }
     }
 }
